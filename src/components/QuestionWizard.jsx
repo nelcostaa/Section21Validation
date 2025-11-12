4b4c869cafbb778d4559a0ef0199fdb7d4e9866a
nelcostaa <nelcostaa@hotmail.com>
2025-11-12 15:20:33 -0300

    style: use site beige background for assessment and result pages

--- src/components/QuestionWizard.jsx
+++ src/components/QuestionWizard.jsx
@@ -153,7 +153,7 @@ const QuestionWizard = ({ onComplete }) => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8 px-4">
+    <div className="min-h-screen bg-simple-beige py-8 px-4">
       <div className="max-w-3xl mx-auto">
         <ProgressIndicator currentQuestionId={state.currentQuestionId} />
         <QuestionCard
